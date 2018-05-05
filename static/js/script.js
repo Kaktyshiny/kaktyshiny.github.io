cc83846dad04d1286bd906ae3cfbf6664ce89154
kaktyshiny <kaktyshiny@gmail.com>
2018-05-05 10:30:29 +0300

    ready

--- static/js/script.js
+++ static/js/script.js
@@ -5,19 +5,19 @@ new Vue({
         menuitems: [
             {
                 name: "First",
-                link: ""
+                link: "#part1"
             },
             {
                 name: "Second",
-                link: ""
+                link: "#part2"
             },
             {
                 name: "Third",
-                link: ""
+                link: "#part3"
             },
             {
                 name: "Fourth",
-                link: ""
+                link: "#footer"
             }
         ]
     }
@@ -38,3 +38,56 @@ $(document).mouseup(function (e){ // событие клика по веб-до�
     }
 });
 
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top - $("nav").height();
+    console.log(destination);
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
+
+new WOW().init();
+
+$("a[href='#part2']").click(function () {
+    new WOW().init();
+});
+
+$(function() {
+    var blockTop1 = $('#part1').offset().top;
+    var blockTop2 = $('#part2').offset().top;
+    var blockTop3 = $('#part3').offset().top;
+    var blockTop4 = $('#footer').offset().top;
+    var $window = $(window);
+    $window.on('load scroll', function() {
+        var top = $window.scrollTop();
+        var height = $window.height();
+        if (top + height >= blockTop1) {
+            $("a[href='#part1']").addClass("active");
+            $("a[href='#part2']").removeClass("active");
+            $("a[href='#part3']").removeClass("active");
+            $("a[href='#footer']").removeClass("active");
+        }
+        if (top + height >= blockTop2) {
+            $("a[href='#part1']").removeClass("active");
+            $("a[href='#part2']").addClass("active");
+            $("a[href='#part3']").removeClass("active");
+            $("a[href='#footer']").removeClass("active");
+        }
+        if (top + height >= blockTop3) {
+            $("a[href='#part1']").removeClass("active");
+            $("a[href='#part2']").removeClass("active");
+            $("a[href='#part3']").addClass("active");
+            $("a[href='#footer']").removeClass("active");
+        }
+        if (top + height >= blockTop4) {
+            $("a[href='#part1']").removeClass("active");
+            $("a[href='#part2']").removeClass("active");
+            $("a[href='#part3']").removeClass("active");
+            $("a[href='#footer']").addClass("active");
+        }
+    });
+});
